2deddd31daa6ed2ba2056a3cd6d0eef024419f60
Matheus Plessmann <maplessmann@gmail.com>
2020-12-19 19:00:54 -0300

    Create navigation component

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import Link from 'components/Link'
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <Link to="/submit">Submit</Link>
+    </nav>
+  )
+}
+
+export default Navigation
